7e3c4fc4e3e76e688d789d7dcb77d3d99c3f27b6
Santiago Torres G <thdealer@gmail.com>
2020-03-02 17:33:01 -0500

    Add Trimmer Sanitizer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  def sanitize_params(params)
+    params.values.each do |str|
+      str.squish! if str.is_a? String
+    end
+    params
+  end
 end
